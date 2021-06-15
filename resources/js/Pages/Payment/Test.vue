f0fa64db4865bc06629d361823e795eebaec24db
Arcangel Higuera <ahiguerl@gmail.com>
2021-06-14 21:58:52 -0500

    Instalación Jetstream

---
+++ resources/js/Pages/Payment/Test.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+       
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            
+
+            <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                
+                <button @click="start()" class="p-4 mx-auto">
+                    Boton de pruebas
+                </button>
+                
+            </div>
+
+           
+        </div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        props: {
+           
+        }
+    }
+</script>
